9f5ca5c6682e83093e62a66cfd39c682b5a9f641
Tess Monical <tess.monical@gmail.com>
2018-02-09 14:08:00 -0600

    adds project model

---
+++ server/db/models/project.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Project = db.define('project', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  startingWordCount: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  }
+})
+
+module.exports = Project;
+
+Project.prototype.getWordcount = function () {
+  return this.getEntries()
+  .then(entries => {
+    return entries.reduce((acc, entry) => {
+      return acc + entry.wordcount;
+    }, 0)
+    .catch(console.error)
+  });
+}
