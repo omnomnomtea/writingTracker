b4e2df37f2f6d6f988c446e30743623865b2714b
Tess Monical <tess.monical@gmail.com>
2018-02-13 15:43:48 -0600

    adds /api/entries/all

--- server/api/entries.js
+++ server/api/entries.js
@@ -9,11 +9,24 @@ router.get('/:id', (req, res, next) => {
       id: Number(req.params.id),
       userId: req.user.id,
     },
-    include: [WordcountEntry],
   })
     .then((wordcountEntry) => {
-      if (wordcountEntry) return res.json(WordcountEntry);
+      if (wordcountEntry) return res.json(wordcountEntry);
       else res.send(404);
+    })
+    .catch(next);
+})
+
+router.get('/all', (req, res, next) => {
+  if (!req.user.id) return res.send(403);
+  WordcountEntry.findAll({
+    where: {
+      userId: req.user.id,
+    },
+  })
+    .then((wordcountEntries) => {
+      if (wordcountEntries) return res.json(wordcountEntries);
+      else res.send([]);
     })
     .catch(next);
 })
